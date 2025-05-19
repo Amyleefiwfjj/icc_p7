7d21d9375657067aed4ddb5a25514cb71636fdbb
이다은 <162148102+Amyleefiwfjj@users.noreply.github.com>
2025-05-19 15:22:06 +0900

    piano page reset button added

--- piano.js
+++ piano.js
@@ -5,7 +5,7 @@ let isRecording = false;
 let recordButton;
 let trail = []; // 브러쉬 트레일 저장용
 const maxTrail = 30;
-let brushcolor=[[240, 158, 158],[237, 183, 145],[237, 231, 145],[191, 245, 159],[158, 240, 202],[156, 230, 225],[163, 208, 240],[184, 163, 240]];
+let brushcolor = [[240, 158, 158], [237, 183, 145], [237, 231, 145], [191, 245, 159], [158, 240, 202], [156, 230, 225], [163, 208, 240], [184, 163, 240]];
 function preload() {
   soundFormats('mp3', 'wav');
   piano = loadSound('./assets/piano_sound.wav');
@@ -15,7 +15,7 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   background(255);
   recordButton = select('#recordButton');
-    prevColor = color(...brushcolor[0]);  // 초기 색상
+  prevColor = color(...brushcolor[0]);  // 초기 색상
   targetColor = color(...brushcolor[0]);
 }
 
@@ -30,10 +30,8 @@ function draw() {
     let size = map(mouseX, 0, width, 5, 50);
     strokeWeight(size);
 
-    // 현재 목표 색상 설정
     targetColor = color(...brushcolor[noteIndex]);
 
-    // 매 프레임마다 부드럽게 색 전환
     prevColor = lerpColor(prevColor, targetColor, 0.05);
 
     stroke(prevColor);
@@ -44,7 +42,7 @@ function draw() {
     if (isRecording) {
       recording.push({ note: notes[noteIndex], volume: volume, time: millis() });
     }
-    
+
     prevNoteIndex = noteIndex;
   }
 }
@@ -72,31 +70,34 @@ function toggleRecording() {
     console.log("Recording stopped. Stored notes:", recording);
   }
 }
+function resetAll() {
+  background(255);
+}
 
-    function randomMix() {
-    isPlayingRandom = true;
-    background(255);
+function randomMix() {
+  isPlayingRandom = true;
+  background(255);
 
-    if (recording.length > 0) {
-        let shuffled = shuffle([...recording]);  // 순서만 섞기
+  if (recording.length > 0) {
+    let shuffled = shuffle([...recording]);  // 순서만 섞기
 
-        for (let i = 0; i < shuffled.length; i++) {
-        setTimeout(() => {
-            let r = shuffled[i];
-            playNote(r.note, r.volume);
+    for (let i = 0; i < shuffled.length; i++) {
+      setTimeout(() => {
+        let r = shuffled[i];
+        playNote(r.note, r.volume);
 
-            // 시각화
-            stroke(color(...brushcolor[getNoteIndexFromMidi(r.note)]));
-            strokeWeight(10);
-            point(r.x, r.y);  // 해당 위치에 점을 찍음
+        // 시각화
+        stroke(color(...brushcolor[getNoteIndexFromMidi(r.note)]));
+        strokeWeight(10);
+        point(r.x, r.y);  // 해당 위치에 점을 찍음
 
-            if (i === shuffled.length - 1) {
-            isPlayingRandom = false;
-            }
-        }, i * 150);
+        if (i === shuffled.length - 1) {
+          isPlayingRandom = false;
         }
-    } else {
-        isPlayingRandom = false;
+      }, i * 150);
     }
+  } else {
+    isPlayingRandom = false;
+  }
 
 }
